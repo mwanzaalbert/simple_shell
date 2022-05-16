11212f4757e7db6408a2a826caa6dc8fb665eb22
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-05-16 06:31:54 -0700

    Commit main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - principal function to run the shell
+*Return: 0 on exit, 1 if any failures happen
+*/
+
+int main(void)
+{
+	printf("Betty's proud.\n");
+	return (0);
+}
