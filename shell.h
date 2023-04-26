aae4622dc7f55e0a575259a85b8f5004d384a41a
mwanzaalbert <71031723+mwanzaalbert@users.noreply.github.com>
2023-04-26 12:52:42 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,10 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define MAX_INPUT_LENGTH 1024
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+
+void print_prompt();
+void read_input(char *input);
+void parse_input(char *input, char **args);
+void execute_command(char **args);
 
 #endif
